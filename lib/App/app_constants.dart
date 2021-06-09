ac6b0f91b447de1fe27e238b6238e1702972673a
Leonardo Viana Oliveira <leonardoviana098@gmail.com>
2021-06-09 16:32:21 -0300

    Classe com contantes das cores utilizadas no app

---
+++ lib/App/app_constants.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static const darkGrey = Color(0xff212121);
+  static const black1 = Color(0xff131313);
+  static const black2 = Color(0xff060606);
+}
