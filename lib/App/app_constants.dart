c17606db2c2e6750c661ab12734ae58089af4cd5
Leonardo Viana Oliveira <leonardoviana098@gmail.com>
2021-06-10 09:07:05 -0300

    Adicionado classe KeyStrings com constantes para teclas

--- lib/App/app_constants.dart
+++ lib/App/app_constants.dart
@@ -4,4 +4,33 @@ class AppColors {
   static const darkGrey = Color(0xff212121);
   static const black1 = Color(0xff131313);
   static const black2 = Color(0xff060606);
+  static const blueGrey = Color(0xFF234a64);
+}
+
+class KeyStrings {
+  static const clear = 'C';
+  static const clearAll = 'AC';
+  static const percent = '%';
+  static const divide = '/';
+  static const multiply = 'X';
+  static const subtract = '-';
+  static const add = '+';
+  static const equals = '=';
+  static const decimal = '.';
+  static const delete = 'DEL';
+  static const inverse = '1/x';
+  static const powerOfTwo = 'x^2';
+  static const squareRoot = '√x';
+  static const negate = '+/-';
+
+  static const zero = '0';
+  static const one = '1';
+  static const two = '2';
+  static const three = '3';
+  static const four = '4';
+  static const five = '5';
+  static const six = '6';
+  static const seven = '7';
+  static const eight = '8';
+  static const nine = '9';
 }
