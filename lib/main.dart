3fc742f16971248141fbb5ee64a90ff83d8d48fe
Leonardo Viana Oliveira <leonardoviana098@gmail.com>
2021-06-11 17:02:18 -0300

    Adicionado provider

--- lib/main.dart
+++ lib/main.dart
@@ -1,29 +1,39 @@
-import 'package:calculator/App/app_theme.dart';
-import 'package:calculator/features/calulator/logic/handle_click.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
-import 'Features/Calulator/UI/calculator_screen.dart';
+import 'app/app_theme.dart';
+import 'features/calulator/logic/handle_click.dart';
+import 'features/calulator/ui/calculator_screen.dart';
+import 'services/firebase_auth_service.dart';
 
-void main() {
-  runApp(
-    MultiProvider(
-      providers: [
-        ChangeNotifierProvider(create: (_) => HandleClicks()),
-      ],
-      child: MyApp(),
-    ),
-  );
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+  runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      title: 'Calculadora 2000',
-      theme: AppTheme.theme,
-      home: CalculatorScreen(),
+    return MultiProvider(
+      providers: [
+        ChangeNotifierProvider(create: (_) => HandleClicks()),
+        Provider<FirebaseAuthService>(
+          create: (_) => FirebaseAuthService(FirebaseAuth.instance),
+        ),
+        StreamProvider(
+          create: (context) => context.read<FirebaseAuthService>().authState,
+          initialData: null,
+        ),
+      ],
+      child: MaterialApp(
+        debugShowCheckedModeBanner: false,
+        title: 'Calculadora 2000',
+        theme: AppTheme.theme,
+        home: CalculatorScreen(),
+      ),
     );
   }
 }
