21d636ee3ebc0443ebc84cc6751482f1a5bba8ba
Leonardo Viana Oliveira <leonardoviana098@gmail.com>
2021-06-11 17:10:38 -0300

    Componete representando os botões da calculadora

--- lib/features/calulator/ui/components/calculator_key.dart
+++ lib/features/calulator/ui/components/calculator_key.dart
@@ -1,22 +1,31 @@
 import 'package:calculator/App/app_constants.dart';
 import 'package:calculator/features/calulator/logic/handle_click.dart';
-
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
 class CalculatorKey extends StatelessWidget {
   final String keyValue;
   final Color keyColor;
+  final int flex;
 
   CalculatorKey({
     Key? key,
     required this.keyValue,
     this.keyColor = AppColors.black1,
+    this.flex = 1,
+  }) : super(key: key);
+
+  CalculatorKey.black({
+    Key? key,
+    required this.keyValue,
+    this.keyColor = AppColors.black2,
+    this.flex = 1,
   }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return Expanded(
+      flex: flex,
       child: Padding(
         padding: const EdgeInsets.all(1.5),
         child: TextButton(
