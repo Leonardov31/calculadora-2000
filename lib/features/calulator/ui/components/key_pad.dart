c196f398c6e3785ff129ce9c05b4c49971504ef7
Leonardo Viana Oliveira <leonardoviana098@gmail.com>
2021-06-11 17:08:54 -0300

    Atualizado

--- lib/features/calulator/ui/components/key_pad.dart
+++ lib/features/calulator/ui/components/key_pad.dart
@@ -1,4 +1,4 @@
-import 'package:calculator/App/app_constants.dart';
+import 'package:calculator/app/app_constants.dart';
 import 'package:flutter/material.dart';
 
 import 'calculator_key.dart';
@@ -19,10 +19,10 @@ class KeyPad extends StatelessWidget {
             child: Row(
               crossAxisAlignment: CrossAxisAlignment.stretch,
               children: [
-                CalculatorKey(keyValue: KeyStrings.percent),
-                CalculatorKey(keyValue: KeyStrings.clearAll),
-                CalculatorKey(keyValue: KeyStrings.clear),
-                CalculatorKey(keyValue: KeyStrings.delete),
+                CalculatorKey.black(keyValue: KeyStrings.allClear),
+                CalculatorKey.black(keyValue: KeyStrings.mod),
+                CalculatorKey.black(keyValue: KeyStrings.delete),
+                CalculatorKey.black(keyValue: KeyStrings.divide),
               ],
             ),
           ),
@@ -30,10 +30,10 @@ class KeyPad extends StatelessWidget {
             child: Row(
               crossAxisAlignment: CrossAxisAlignment.stretch,
               children: [
-                CalculatorKey(keyValue: KeyStrings.inverse),
-                CalculatorKey(keyValue: KeyStrings.powerOfTwo),
-                CalculatorKey(keyValue: KeyStrings.squareRoot),
-                CalculatorKey(keyValue: KeyStrings.divide),
+                CalculatorKey(keyValue: KeyStrings.seven),
+                CalculatorKey(keyValue: KeyStrings.eight),
+                CalculatorKey(keyValue: KeyStrings.nine),
+                CalculatorKey.black(keyValue: KeyStrings.multiply),
               ],
             ),
           ),
@@ -41,19 +41,10 @@ class KeyPad extends StatelessWidget {
             child: Row(
               crossAxisAlignment: CrossAxisAlignment.stretch,
               children: [
-                CalculatorKey(
-                  keyValue: KeyStrings.seven,
-                  keyColor: Colors.black87,
-                ),
-                CalculatorKey(
-                  keyValue: KeyStrings.eight,
-                  keyColor: Colors.black87,
-                ),
-                CalculatorKey(
-                  keyValue: KeyStrings.nine,
-                  keyColor: Colors.black87,
-                ),
-                CalculatorKey(keyValue: KeyStrings.multiply),
+                CalculatorKey(keyValue: KeyStrings.four),
+                CalculatorKey(keyValue: KeyStrings.five),
+                CalculatorKey(keyValue: KeyStrings.six),
+                CalculatorKey.black(keyValue: KeyStrings.subtract),
               ],
             ),
           ),
@@ -61,19 +52,10 @@ class KeyPad extends StatelessWidget {
             child: Row(
               crossAxisAlignment: CrossAxisAlignment.stretch,
               children: [
-                CalculatorKey(
-                  keyValue: KeyStrings.four,
-                  keyColor: Colors.black87,
-                ),
-                CalculatorKey(
-                  keyValue: KeyStrings.five,
-                  keyColor: Colors.black87,
-                ),
-                CalculatorKey(
-                  keyValue: KeyStrings.six,
-                  keyColor: Colors.black87,
-                ),
-                CalculatorKey(keyValue: KeyStrings.subtract),
+                CalculatorKey(keyValue: KeyStrings.one),
+                CalculatorKey(keyValue: KeyStrings.two),
+                CalculatorKey(keyValue: KeyStrings.three),
+                CalculatorKey.black(keyValue: KeyStrings.add),
               ],
             ),
           ),
@@ -81,38 +63,8 @@ class KeyPad extends StatelessWidget {
             child: Row(
               crossAxisAlignment: CrossAxisAlignment.stretch,
               children: [
-                CalculatorKey(
-                  keyValue: KeyStrings.one,
-                  keyColor: Colors.black87,
-                ),
-                CalculatorKey(
-                  keyValue: KeyStrings.two,
-                  keyColor: Colors.black87,
-                ),
-                CalculatorKey(
-                  keyValue: KeyStrings.three,
-                  keyColor: Colors.black87,
-                ),
-                CalculatorKey(keyValue: KeyStrings.add),
-              ],
-            ),
-          ),
-          Expanded(
-            child: Row(
-              crossAxisAlignment: CrossAxisAlignment.stretch,
-              children: [
-                CalculatorKey(
-                  keyValue: KeyStrings.negate,
-                  keyColor: Colors.black87,
-                ),
-                CalculatorKey(
-                  keyValue: KeyStrings.zero,
-                  keyColor: Colors.black87,
-                ),
-                CalculatorKey(
-                  keyValue: KeyStrings.decimal,
-                  keyColor: Colors.black87,
-                ),
+                CalculatorKey(flex: 2, keyValue: KeyStrings.zero),
+                CalculatorKey(keyValue: KeyStrings.decimal),
                 CalculatorKey(
                   keyValue: KeyStrings.equals,
                   keyColor: AppColors.blueGrey,
