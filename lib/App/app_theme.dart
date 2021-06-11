2662aa21f4ce0a861fd89eeb3c6cb018d82d94fe
Leonardo Viana Oliveira <leonardoviana098@gmail.com>
2021-06-11 16:58:28 -0300

    Estilo global do TextFormFiedl; Estilo global do ElevatedButton

--- lib/App/app_theme.dart
+++ lib/App/app_theme.dart
@@ -1,14 +1,31 @@
-import 'package:calculator/App/app_constants.dart';
+import 'package:calculator/app/app_constants.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 class AppTheme {
   static final theme = ThemeData(
+    primaryColor: AppColors.darkGrey,
     brightness: Brightness.dark,
     scaffoldBackgroundColor: AppColors.darkGrey,
+    inputDecorationTheme: InputDecorationTheme(
+      fillColor: AppColors.darkGrey,
+      focusedBorder: OutlineInputBorder(
+        borderRadius: BorderRadius.zero,
+        borderSide: BorderSide(color: AppColors.darkGrey, width: 1.5),
+      ),
+    ),
     appBarTheme: AppBarTheme(
       elevation: 0,
       backgroundColor: AppColors.darkGrey,
+    ),
+    elevatedButtonTheme: ElevatedButtonThemeData(
+      style: ElevatedButton.styleFrom(
+        primary: AppColors.blueGrey,
+        padding: EdgeInsets.all(20.0),
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.zero,
+        ),
+      ),
     ),
     textButtonTheme: TextButtonThemeData(
       style: TextButton.styleFrom(
